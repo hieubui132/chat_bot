90d6ba66c1b2100fced4912c61a0fc569cc13f65
Bùi Hiếu <buichihieu1321999@gmail.com>
2024-06-13 11:17:46 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8080);
 
   if (module.hot) {
     module.hot.accept();
