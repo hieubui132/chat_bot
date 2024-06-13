b9c4fee77078bdeedcade8d0a1c0b76c5b5d19f4
Bùi Hiếu <buichihieu1321999@gmail.com>
2024-06-13 11:26:11 +0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,34 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WebHookModule } from './modules/webhook/webHook.module';
-import { TeleModule } from './modules/tele/tele.module';
-import configuration from './config/configuration';
-import { ScheduleModule } from '@nestjs/schedule';
-import { JobModule } from './modules/job/job.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { conversation, employee, subject } from './entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Tùy chọn này sẽ làm cho ConfigModule có sẵn ở mọi nơi trong ứng dụng
       envFilePath: '.env',
-      load: [configuration],
     }),
-    ScheduleModule.forRoot(),
     WebHookModule,
-    // TeleModule,
-    // JobModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: '117.103.224.26',
-    //   port: 5432,
-    //   username: 'postgres',
-    //   password: 'Khongbiet098',
-    //   database: 'messenger',
-    //   entities: [conversation, employee, subject],
-    //   synchronize: true,
-    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
